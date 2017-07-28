6ec3d3ec7b076cc7e24b9be5d7c4f1637b0c5fa9
P.Y. Laligand <py.laligand@gmail.com>
2017-07-28 23:08:28 +0200

    Adjust to API change (#12)

--- lib/dummy_handler.dart
+++ lib/dummy_handler.dart
@@ -3,15 +3,13 @@
 import 'dart:async';
 
 import 'package:heroku_slack_bot/heroku_slack_bot.dart';
-import 'package:logging/logging.dart';
-import 'package:shelf/shelf.dart' as shelf;
 
 /// Does not do anything useful, really.
 class DummyHandler extends SlackCommandHandler {
   final Logger _log = new Logger('DummyHandler');
 
   @override
-  Future<shelf.Response> handle(shelf.Request request) async {
+  Future<Response> handle(Request request) async {
     final params = request.context;
     final String userName = params[SLACK_USERNAME];
     _log.info('Request from $userName');
